ac71c08c0dac170d563011eaaa8ba6f445d87ad9
Tony <jinniw43805@gmail.com>
2015-03-11 21:45:14 +0800

    Initial Commit

---
+++ TKU-HomeWork/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  TKU-HomeWork
+//
+//  Created by 郭彥君 on 15/3/11.
+//  Copyright (c) 2015年 郭彥君. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
